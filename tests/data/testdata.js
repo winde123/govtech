3612437d768b126ddbd406cb8db17026b97e6233
ed <edwinwan88@gmail.com>
2026-01-10 01:33:44 +0800

    added url name generator

--- tests/data/testdata.js
+++ tests/data/testdata.js
@@ -1,2 +1,5 @@
 export const situser = 'test_edwin@gowhere.gov.sg'
 export const wronguser = 'test'
+export const random_website_name = () =>{
+    return 'testurl' + Math.floor((Math.random())*1000).toString()
+}
