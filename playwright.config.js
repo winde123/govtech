5479bdc51b2465d5c077df4f8190393dc103c815
ed <edwinwan88@gmail.com>
2026-01-10 01:34:58 +0800

    added longer timeout

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,9 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout:60000,
   use: {
+    actionTimeout: 60000,
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://uat.diy.gowhere.gov.sg/home',
 
